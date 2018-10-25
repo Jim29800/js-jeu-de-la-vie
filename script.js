480d9fcb603acb54a94a5a098d15705c4c018cc6
Jimmy <jbouvier1173@gmail.com>
2018-10-25 16:40:55 +0200

    Nouveau jdlv

--- script.js
+++ script.js
@@ -1,244 +1,248 @@
-var ligne = 12;
-var col = 50;
-var tableau = [];
-//generation du tableau HTML et JS
-var cible = $("#cible");
-var html;
-for (var i = 0; i < ligne; i++) {
-    html += "<tr id="+i+">";
-    tableau.push([])
-    for (var j = 0; j < col; j++) {
-        tableau[i].push(0);
-        html += "<td class="+j+"></td>";
+//------------------------------------Définition des variables----------------------------------------
+
+//table
+let ligne = 5;
+let col = 10;
+let cible = $("#cible");    //cible la table du HTML pour créer row et col
+let html;                   //contient le code html pour créer la table
+let damier = [];
+
+//fourmis
+let listeCouleurs = ["bleu", "rouge"]   //à ajouter égallement au CSS
+let nombreFourmisTotal = 2;            //doit être divisible par le nombre de couleur 
+let nombreFourmisParCouleur = nombreFourmisTotal / listeCouleurs.length;
+let fourmisVivantes = [];
+let fourmisMortes = [];
+
+//durée de vie d'une fourmi : [ mini , maxi ]
+let dureeDeVie = [30,35]
+
+//accouplement
+let ageMin = 5;
+let ageMax = 25;
+let delais = 2;
+//----------------------generation de la table HTML et du damier (array) JS----------------------------------------
+
+//Génération du code HTML
+for (let i = 0; i < ligne; i++) {
+    html += "<tr id=" + i + ">";
+    damier.push([])
+    for (let j = 0; j < col; j++) {
+        damier[i].push(0);
+        html += "<td class=" + j + "></td>";
     }
     html += "</tr>";
 }
+
+//Injection du code dans la page
 cible.html(html);
-//test pour target
-$("td").click(function(){
-    var test = $(this).attr("class");
-    var test2 = $(this).parent().attr("id");
-    alert("ligne : "+test2+" colone : "+test)
-})
-//placement itinitial
-//emplacement vide : 0 
-//---------------------------------------------COULEUR----------------------------------------
-var bleu = 1;
-var rouge = 2;
-//emplacement accouplement bleu : 3
-var accouplement_bleu =  "bleur";
-//emplacement accouplement rouge : 4
-var accouplement_rouge =  "rouger";     
-//emplacement combat : 5
-var combat = "fight";
 
-function insert_couleur(couleur){
-    if (couleur == 1) {
-        return "bleu";
-    }else if (couleur == 2) {
-        return "rouge";
+//test pour cibler les cases
+/* $("td").click(function () {
+    let classe = $(this).attr("class");
+    let id = $(this).parent().attr("id");
+    console.log("ligne : " + id + " colone : " + classe)
+}) */
+
+//---------------------------------------------CLASSE----------------------------------------
+
+class Fourmi {
+    constructor(couleur, sexe = this.setSexe(), dureeDeVieMin = dureeDeVie[0], dureeDeVieMax = dureeDeVie[1]) {
+        this.couleur = couleur;
+        this.sexe = sexe;
+        this.age = 0;
+        this.ageMax = this.setAgeMax(dureeDeVieMin, dureeDeVieMax);
+        this.morte = false;
+        this.x;
+        this.y;
+    }
+    setSexe(){
+        let result = Math.floor(Math.random() * 2);
+        if (result === 0) {
+            return "F";
+        }else{
+            return "M";
+        }
+    }
+    setAgeMax(dureeDeVieMin, dureeDeVieMax){
+        let interval = dureeDeVieMax - dureeDeVieMin + 1;
+        let result = dureeDeVieMin + Math.floor(Math.random() * interval);
+        return result; 
     }
 }
 
+//--------------------------------------Génération des fourmis----------------------------------------
 
-
-function alea(chiffre){
-    return Math.floor(Math.random() * chiffre); 
-};
-
-function depart (nb, type, tab = tableau){
-    compteur = 0;
-    while (compteur < nb) {
-        var alea_ligne = alea(ligne);
-        var alea_col = alea(col);        
-        if (tab[alea_ligne][alea_col] === 0) {
-            $("#"+alea_ligne+" > ."+alea_col).attr("couleur", insert_couleur(type));
-            tab[alea_ligne][alea_col] = type;
-            //console.log("ligne :"+ alea_ligne + " col : " +alea_col);
-            compteur++;
+//pour chaque couleurs
+for (let i = 0; i < listeCouleurs.length; i++) {
+    //créer le nombre de fourmis par couleur
+    for (let j = 0; j < nombreFourmisParCouleur; j++) {
+        //avec au minimum un M et une F
+        if (j === 0) {
+            fourmisVivantes.push(new Fourmi(listeCouleurs[i], "F"));
+        }else if (j === 1) {
+            fourmisVivantes.push(new Fourmi(listeCouleurs[i], "M"));
+        }else {
+            fourmisVivantes.push(new Fourmi(listeCouleurs[i]));
         }
     }
-
 }
-//----------------------------------------------------------------------------------------------------------------------
-//----------------------------------------------------------------------------------------------------------------------
-//Nombre de pions -------------------------------------------------------------------------------------------------
-depart(8, bleu)
-depart(8, rouge)
-//----------------------------------------------------------------------------------------------------------------------
-//----------------------------------------------------------------------------------------------------------------------
-//----------------------------------------------------------------------------------------------------------------------
-function deplacement_calcul(actuel, max){ 
-    if (actuel == 0) {
-        if (Math.floor(Math.random() * 2 + 1) == 1) {
-            //reste sur la position
-            return actuel;
-        }else{
-            //monte d'un cran
-            return actuel + 1;
-        }
-    }else if (actuel == max-1) {
-        if (Math.floor(Math.random() * 2 + 1) == 1) {
-            //reste sur la position
-            return actuel;
-        }else{
-            //descend d'un cran
-            return actuel - 1;
-        }
+
+//---------------------------------Positionnement initial des fourmis----------------------------------------
+
+function positionInitial(fourmisAPlacer = fourmisVivantes, terrain = damier, nbLigne = ligne, nbCol = col) {
+    // compte le nombre de fourmis en place
+    let compteurFourmis = 0;
+    //verifie qu'il y a suffisament d'emplacement
+    if (fourmisAPlacer.length >= (terrain.length * terrain[0].length) ) {
+        alert("Il y a trop de fourmis !")
     }else {
-        var result = Math.floor(Math.random() * 3 + 1); 
-        if (result == 1) {
-            //reste sur la position
-            return actuel;
-        }else if (result == 2) {
-            //monte d'un cran
-            return actuel + 1;
+        while (compteurFourmis < fourmisAPlacer.length) {
+            let ligne = Math.floor(Math.random() * nbLigne);
+            let col = Math.floor(Math.random() * nbCol);
+            if (terrain[ligne][col] === 0) {
+                //place la fourmis sur le terrain
+                $("#" + ligne + " > ." + col).attr("couleur", fourmisAPlacer[compteurFourmis].couleur );
+                terrain[ligne][col] = fourmisAPlacer[compteurFourmis];
+                //ajoute les coordonnées à la fourmi
+                fourmisAPlacer[compteurFourmis].x = ligne;
+                fourmisAPlacer[compteurFourmis].y = col;
+                //incrémante le compteur
+                compteurFourmis++;
+            }
         }
-        else{
-            //descend d'un cran
-            return actuel - 1;
-        }
     }
-    
-}
-function numero_ennemi(num_actuel){
-    if (num_actuel == 1) {
-        return 2;
-    }
-    if (num_actuel == 2) {
-        return 1;
-    }
-};
-function combatre() {
-    return Math.floor(Math.random() * 2 + 1); 
-}
 
-function deplacement(tab){
-    Array.prototype.clone = function() {
-        var newArray = (this instanceof Array) ? [] : {};
-        for (i in this) {
-        if (i == 'clone') continue;
-        if (this[i] && typeof this[i] == "object") {
-            newArray[i] = this[i].clone();
-        } else newArray[i] = this[i]
-        } return newArray;
-    }
-    var tab_nouveau = tab.clone();    
-    for (var i = 0; i < tab.length; i++) {
-        for (var j = 0; j < tab[i].length; j++) {
-            if (tab[i][j] == 1 || tab[i][j] == 2) {
-                console.log("oui")
-                var valeur = tab_nouveau[i][j];
-                var ennemi = numero_ennemi(valeur);
-                var ligne_nouvelle = deplacement_calcul(i, ligne);
-                var col_nouvelle = deplacement_calcul(j, col);
-                //libere la case actuelle JS
-                tab_nouveau[i][j] = 0;
-                if (tab_nouveau[ligne_nouvelle][col_nouvelle] == 0) { 
-                    //DEPLACEMENT ----------------------
-                    //pop sur nouvelle case JS
-                    tab_nouveau[ligne_nouvelle][col_nouvelle] = valeur;
-                }else if (tab_nouveau[ligne_nouvelle][col_nouvelle] == valeur) {
-                    //ACCOUPLEMENT ----------------------
-                    if (valeur == 1) {
-                        //pop sur nouvelle case JS
-                        tab_nouveau[ligne_nouvelle][col_nouvelle] = 3;
-                    }else if (valeur == 2) {
-                        //pop sur nouvelle case JS
-                        tab_nouveau[ligne_nouvelle][col_nouvelle] = 4;
-                    }
-                }else if (tab_nouveau[ligne_nouvelle][col_nouvelle] == ennemi) {
-                    //COMBAT ----------------------
-                    //pop sur nouvelle case JS
-                    tab_nouveau[ligne_nouvelle][col_nouvelle] = 5;
-                }else{
-                    //si un combat ou un accouplement a lieu sur la destination, alors reste sur place
-                    //pop sur nouvelle case JS
-                    tab_nouveau[i][j] = valeur;
-                }
-            }
-            else if (tab[i][j] == 3) {
-                tab_nouveau[i][j] = 0;
-                compteur = 0;
-                while (compteur < 3) {
-                    var alea_ligne = alea(ligne);
-                    var alea_col = alea(col);        
-                    if (tab_nouveau[alea_ligne][alea_col] == 0) {
-                        tab_nouveau[alea_ligne][alea_col] = 1;
-                        compteur++;
-                    }
-                }
-            }
-            else if (tab[i][j] == 4) {
-                tab_nouveau[i][j] = 0;                
-                compteur = 0;
-                while (compteur < 3) {
-                    var alea_ligne = alea(ligne);
-                    var alea_col = alea(col);        
-                    if (tab_nouveau[alea_ligne][alea_col] == 0) {
-                        tab_nouveau[alea_ligne][alea_col] = 2;
-                        compteur++;
-                    }
-                }
-            }
-            else if (tab[i][j] == 5) {
-                tab_nouveau[i][j] = combatre();
-            }
-        }
-    }
-    console.log(tab_nouveau)
-    return tab_nouveau;
 }
-function affiche_deplacement(tab_nouveau = tableau){
-    var compteur1 = 0;
-    var compteur2 = 0;
-    var toutecase = 0;
-    for (var i = 0; i < tab_nouveau.length; i++) {
-        for (var j = 0; j < tab_nouveau[i].length; j++) {
-            if(tab_nouveau[i][j] == 0) {
-                toutecase = 1;
-                $("#"+i+" > ."+j).removeAttr("couleur");
-            }
-            if(tab_nouveau[i][j] == 1) {
-                compteur1++;
-                $("#"+i+" > ."+j).attr("couleur", insert_couleur(1));
-            }
-            if(tab_nouveau[i][j] == 2) {
-                compteur2++
-                $("#"+i+" > ."+j).attr("couleur", insert_couleur(2));
-            }
-            if(tab_nouveau[i][j] == 3) {
-                compteur1++;                
-                $("#"+i+" > ."+j).attr("couleur", accouplement_bleu);
-            }
-            if(tab_nouveau[i][j] == 4) {
-                compteur2++                
-                $("#"+i+" > ."+j).attr("couleur", accouplement_rouge);
-            }
-            if(tab_nouveau[i][j] == 5) {
-                compteur1++;                
-                compteur2++;                                
-                $("#"+i+" > ."+j).attr("couleur", combat);
-            }
-        }
-    }
-    if(compteur1 == 0) {
-        clearInterval(auto);
-        alert("Rouge domine");
-    }else if(compteur2 == 0) {
-        clearInterval(auto);
-        alert("Bleu domine");
-    }
-    else if(toutecase == 0) {
-        clearInterval(auto);
-        alert("Plus de mouvement possible");
+positionInitial();
+
+//---------------------------------Déplacement des fourmis----------------------------------------
+
+//retourne un bool si la fourmis est vivante et mofifie son age
+function estVivante(fourmi) {
+    if (fourmi.ageMax === fourmi.age) {
+        fourmi.morte = true;
+        return false;
+    }else{
+        fourmi.age ++;
+        return true;
     }
 }
 
-    
-//console.log(tableau)
-//debut de l'automatisation
+//retourne un tableau de la nouvelle position [x, y]
+function calculDeplacement(fourmi, terrain = damier){
+
+    //dimensions du terrain
+    xTerrainMin = 0;
+    xTerrainMax = terrain.length -1;
+    yTerrainMin = 0;
+    yTerrainMax = terrain[0].length - 1;
+
+    //position actuel de la fourmis
+    let xActuel = fourmi.x;
+    let yActuel = fourmi.y;
+
+    //jet random pour futur position
+    let xRandom = Math.floor(Math.random() * 3);
+    let yRandom = Math.floor(Math.random() * 3);
+
+    //définition du déplacement en fonction du random
+    let xDeplace;
+    let yDeplace;
+    // x
+    if (xRandom === 2) {
+        xDeplace = -1;
+    }else{
+        xDeplace = xRandom;
+    }
+    // y
+    if (yRandom === 2) {
+        yDeplace = -1;
+    } else {
+        yDeplace = yRandom;
+    }
+
+    // calcul du déplacement sans tenir compte des bords
+    let xCalculDeplacement = xActuel + xDeplace;
+    let yCalculDeplacement = yActuel + yDeplace;
+
+    //nouvelle position
+    let xNouveau;
+    let yNouveau;
+
+    //vérification par rapport aux bords
+    // x
+    if (xCalculDeplacement > xTerrainMax) {
+        xNouveau = xTerrainMin;
+    } else if (xCalculDeplacement < xTerrainMin) {
+        xNouveau = xTerrainMax;
+    } else {
+        xNouveau = xCalculDeplacement;
+    }
+    // y
+    if (yCalculDeplacement > yTerrainMax) {
+        yNouveau = yTerrainMin;
+    } else if (yCalculDeplacement < yTerrainMin) {
+        yNouveau = yTerrainMax;
+    } else {
+        yNouveau = yCalculDeplacement;
+    }
+
+    return [xNouveau, yNouveau];
+}
+
+function deplacement(vivantes = fourmisVivantes, mortes = fourmisMortes , terrain = damier){
+    for (let i = 0; i < vivantes.length; i++) {
+
+        //vérifie son age
+        if(estVivante(vivantes[i])){
+
+            // obtention des nouvelles coordonnées
+            let nouvelleCoordonees = calculDeplacement(vivantes[i]);
+            let x = nouvelleCoordonees[0];
+            let y = nouvelleCoordonees[1];
+
+            //verifie que la fourmi a changer de place
+            if (x === vivantes[i].x && y === vivantes[i].y) {
+                //ne fait rien
+            }
+            //verifie que l'emplacement est vide
+            else if (terrain[x][y] === 0) {
+                
+                //déplace la fourmi sur la nouvelle place
+                terrain[x][y] = vivantes[i];
+
+                //efface l'ancienne position sur la page html
+                $("#" + vivantes[i].x + " > ." + vivantes[i].y).removeAttr("couleur");
+
+                //nettoye l'ancienne place
+                terrain[vivantes[i].x][vivantes[i].y] = 0;
+
+                //renseigne la nouvelle position à la fourmi
+                terrain[x][y].x = x;
+                terrain[x][y].y = y;
+
+                //ajoute la nouvelle position sur la page html
+                $("#" + x + " > ." + y).attr("couleur", vivantes[i].couleur);
+
+            }
+        }else{
+            $("#" + vivantes[i].x + " > ." + vivantes[i].y).removeAttr("couleur");
+            mortes.push(vivantes[i]);
+            terrain[vivantes[i].x][vivantes[i].y] = 0;
+            vivantes.slice(i, 1);
+        }
+    }
+
+    //stop le set interval si il n'y a plus de fourmis
+    if (vivantes.length === 0) {
+        clearInterval(auto);
+    }
+}
+
+
+
 var auto = setInterval(function () {
-    tableau = deplacement(tableau)
-    affiche_deplacement()
+    deplacement();
+    console.log(damier)
 }, 1000);
